71a744595596ab72f5a85fcd8f24bb95964c7714
AlainaMarta <alainamarta@gmail.com>
2024-10-19 14:26:38 -0300

    implementa e refatora função de favoritar

--- src/pages/Album/CardMusic.tsx
+++ src/pages/Album/CardMusic.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import { SongType } from '../../types';
+import { addSong, getFavoriteSongs, removeSong } from '../../services/favoriteSongsAPI';
 
 function MusicCard({ trackId, trackName, previewUrl }: SongType) {
   const [isFavorite, setIsFavorite] = useState(false);
 
-  function handleFavoriteChange() {
-    setIsFavorite(!isFavorite);
-    console.log('foi');
+  async function handleFavoriteChange() {
+    const favoriteSongs = await getFavoriteSongs();
+    const isSongFavorite = favoriteSongs.some((song) => song.trackId === trackId);
+
+    if (isSongFavorite) {
+      await removeSong({ trackId, trackName, previewUrl });
+    } else {
+      await addSong({ trackId, trackName, previewUrl });
+    }
+    const updatedFavoriteSongs = await getFavoriteSongs();
+    setIsFavorite(updatedFavoriteSongs.some((song) => song.trackId === trackId));
   }
 
   return (
@@ -20,13 +29,14 @@ function MusicCard({ trackId, trackName, previewUrl }: SongType) {
         <code>audio</code>
         .
       </audio>
-      <label htmlFor="heart">
+      <label style={ { cursor: 'pointer' } }>
         <input
           type="checkbox"
           name="heart"
           checked={ isFavorite }
           id="heart"
           onChange={ handleFavoriteChange }
+          style={ { display: 'none' } }
         />
         <img
           src={ isFavorite
