0c60698b4e68ff600a27d45c9ddec44b36b81184
AlainaMarta <alainamarta@gmail.com>
2024-11-05 21:44:23 -0300

    exibe informações do usuario

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserType } from '../../types';
+import { getUser } from '../../services/userAPI';
+import LoadingMessage from '../Login/Loading';
+
 function Profile() {
+  const [userInfo, setUserinfo] = useState<UserType>();
+  const [loading, setLoading] = useState<boolean>();
+
+  useEffect(() => {
+    async function handleUserInfo() {
+      setLoading(true);
+      const getUserInfo = await getUser();
+      setUserinfo(getUserInfo);
+      setLoading(false);
+    }
+    handleUserInfo();
+  }, []);
+
   return (
-    <p>Perfil</p>
+    <div>
+      {loading && <LoadingMessage />}
+      <h1>{userInfo?.name}</h1>
+      <h2>{userInfo?.email}</h2>
+      <h3>{userInfo?.description}</h3>
+      <img src={ userInfo?.image } alt="profile" data-testid="profile-image" />
+      <Link to="/profile/edit">Editar perfil </Link>
+    </div>
   );
 }
 
